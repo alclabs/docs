0e71627a29cfa1e059528794f95b8170ed502afb
John <jmurph.alc@gmail.com>
2012-08-29 20:49:44 -0400

    Created a helper for automatically extracting default resources from the .jar (or .war).

--- src/main/java/com/alcshare/docs/MimeManager.java
+++ src/main/java/com/alcshare/docs/MimeManager.java
@@ -1,6 +1,7 @@
 package com.alcshare.docs;
 
 import com.alcshare.docs.util.AddOnFiles;
+import com.alcshare.docs.util.FileResource;
 import com.alcshare.docs.util.Logging;
 import org.apache.commons.io.IOUtils;
 
@@ -12,51 +13,30 @@ import java.util.Properties;
  */
 public class MimeManager {
     private static final String DEFAULT_MIME_TYPE = "text/plain";
+    private static final FileResource MIME_CONFIG = new FileResource(AddOnFiles.getConfigDirectory(), "mime.properties", MimeManager.class);
 
-    private static Properties mappings = new Properties();
-    private static final String MIME_CONFIG_NAME = "mime.properties";
+    private static final Properties mappings = new Properties();
 
     public static synchronized String getMimeTypeForExtension(String extension) {
-        String result = null;
-        if (mappings != null) {
-            result = (String) mappings.get(extension);
-        }
-        if (result == null) {
-            result = DEFAULT_MIME_TYPE;
-        }
-        return result;
+        String result = (String) mappings.get(extension);
+        return result != null ? result : DEFAULT_MIME_TYPE;
     }
 
     private static synchronized void loadMimeTypes() {
-        File mimeFile = getMimeConfigFile();
         mappings.clear();
 
+        InputStream is = null;
         try {
-            mappings.load(new FileInputStream(mimeFile));
+            is = MIME_CONFIG.openBufferedStream();
+            mappings.load(is);
         } catch (IOException e) {
-            Logging.println("Error loading "+mimeFile.getAbsolutePath(), e);
+            Logging.println("Error loading "+MIME_CONFIG, e);
+        } finally {
+            IOUtils.closeQuietly(is);
         }
-    }
-
-    private static File getMimeConfigFile() {
-        return new File(AddOnFiles.getConfigDirectory(), MIME_CONFIG_NAME);
     }
 
     public static void initialize() {
-        File mimeFile = getMimeConfigFile();
-        if (!mimeFile.exists()) {
-            InputStream is = MimeManager.class.getResourceAsStream(MIME_CONFIG_NAME);
-            try {
-                FileOutputStream os = new FileOutputStream(mimeFile);
-                IOUtils.copy(is, os);
-                os.close();
-            } catch (IOException e) {
-                Logging.println("Error writing default file "+mimeFile.getAbsolutePath(), e);
-            }
-        } else {
-            Logging.println("Mime configuration file '"+mimeFile.getAbsolutePath()+"' is missing");
-        }
-
         loadMimeTypes();
     }
 }
